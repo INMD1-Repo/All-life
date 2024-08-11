a2104231bd334c733f5906ff5b17305df4883440
INMD1 <lyw514549@gmail.com>
2024-08-12 01:00:03 +0900

    프론트 중간 저장

--- Frontend/lib/page/home.dart
+++ Frontend/lib/page/home.dart
@@ -9,15 +9,30 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home Page')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
+      //앱 바는 구현 안
+      // appBar: AppBar(title: Text('테스트 구현 페이')),
+      body: Column(
+        children: [
+          //메뉴바 관련 코드임
+          Container(
+            margin: EdgeInsets.fromLTRB(8, 20, 8, 0),
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 25.0),
+            decoration: BoxDecoration(
+              //컬러는 나중에 수정 예정 레이아웃 작성
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          //콘텐츠 부분
+          Container(
 
-          },
-          child: Text('Go to Details Page'),
-        ),
-      ),
+          ),
+          //하단 메뉴 부
+          Container(
+
+          )
+        ],
+      )
     );
   }
 }
