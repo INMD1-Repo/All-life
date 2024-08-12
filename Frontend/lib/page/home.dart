c1c12bbf32cbeace87520f017660b97bee989447
INMD1 <lyw514549@gmail.com>
2024-08-13 02:14:37 +0900

    페이지 미리 추가

--- Frontend/lib/page/home.dart
+++ Frontend/lib/page/home.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // 현재 선택된 버튼을 추적하는 변수
+  int _selectedIndex = 0;
+
+  // 버튼을 클릭했을 때 호출되는 메서드
+  void _onButtonPressed(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // 앱 바는 구현 안 함
-      // appBar: AppBar(title: Text('테스트 구현 페이지')),
       body: Column(
         children: [
           // 메뉴바 관련 코드
@@ -20,29 +30,28 @@ class HomePage extends StatelessWidget {
               margin: EdgeInsets.fromLTRB(8, 20, 8, 15),
               padding: EdgeInsets.symmetric(horizontal: 10.0),
               decoration: BoxDecoration(
-                // 컬러는 나중에 수정 예정, 레이아웃 작성
                 color: Color(0xffeff3fb),
                 borderRadius: BorderRadius.circular(20),
               ),
               child: Row(
-               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-               children: [
-                 IconButton(
-                   icon: Icon(Icons.menu, color: Colors.grey),
-                   onPressed: () {
-                     // 메뉴 버튼 클릭 시 실행할 코드
-                   },
-                 ),
-                 Text(
-                   'ALL LIFE',
-                   style: TextStyle(color: Colors.black, fontSize: 16.0),
-                 ),
-                 CircleAvatar(
-                   backgroundImage: NetworkImage(
-                     'https://example.com/profile.jpg', // 여기에 프로필 이미지 URL 입력
-                   ),
-                 ),
-               ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.menu, color: Colors.grey),
+                    onPressed: () {
+                      // 메뉴 버튼 클릭 시 실행할 코드
+                    },
+                  ),
+                  Text(
+                    'ALL LIFE',
+                    style: TextStyle(color: Colors.black, fontSize: 16.0),
+                  ),
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      'https://example.com/profile.jpg', // 여기에 프로필 이미지 URL 입력
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
@@ -63,74 +72,16 @@ class HomePage extends StatelessWidget {
           Expanded(
             flex: 1, // 20%
             child: Container(
-              //color: Colors.greenAccent, // 임시로 배경색 지정 (하단 메뉴 영역)
               child: Center(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Padding(
-                        padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
-                        child: TextButton(
-                             onPressed: () {},
-                            style: TextButton.styleFrom(
-                              overlayColor: Colors.transparent, // 눌림 효과 제거
-                            ),
-                        child: Column(
-                          children: [
-                            Icon(Icons.home, color: Colors.black),
-                            Text("홈", style: TextStyle(color: Colors.black))
-                          ],
-                        )
-                      )
-                    ),
-                    Padding(
-                        padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
-                        child: TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                              overlayColor: Colors.transparent, // 눌림 효과 제거
-                            ),
-                            child: Column(
-                              children: [
-                                Icon(Icons.place, color: Colors.black),
-                                Text("지도 보기", style: TextStyle(color: Colors.black))
-                              ],
-                            )
-                        )
-                    ),
-                    Padding(
-                        padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
-                        child: TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                              overlayColor: Colors.transparent, // 눌림 효과 제거
-                            ),
-                            child: Column(
-                              children: [
-                                Icon(Icons.settings, color: Colors.black),
-                                Text("설정", style: TextStyle(color: Colors.black))
-                              ],
-                            )
-                        )
-                    ),
-                    Padding(
-                        padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
-                        child: TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                              overlayColor: Colors.transparent, // 눌림 효과 제거
-                            ),
-                            child: Column(
-                              children: [
-                                Icon(Icons.account_circle, color: Colors.black),
-                                Text("계정", style: TextStyle(color: Colors.black))
-                              ],
-                            )
-                        )
-                    )
+                    _buildButton(0, Icons.home, "홈"),
+                    _buildButton(1, Icons.place, "지도 보기"),
+                    _buildButton(2, Icons.settings, "설정"),
+                    _buildButton(3, Icons.account_circle, "계정"),
                   ],
-
-                )
+                ),
               ),
             ),
           ),
@@ -138,4 +89,31 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+
+  // 버튼을 생성하는 메서드
+  Widget _buildButton(int index, IconData icon, String label) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
+      child: TextButton(
+        onPressed: () => _onButtonPressed(index),
+        style: TextButton.styleFrom(
+          overlayColor: Colors.transparent, // 눌림 효과 제거
+        ),
+        child: Column(
+          children: [
+            Icon(
+              icon,
+              color: _selectedIndex == index ? Colors.blue : Colors.black, // 선택된 버튼이면 파란색, 아니면 검은색
+            ),
+            Text(
+              label,
+              style: TextStyle(
+                color: _selectedIndex == index ? Colors.blue : Colors.black, // 선택된 버튼이면 파란색, 아니면 검은색
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
