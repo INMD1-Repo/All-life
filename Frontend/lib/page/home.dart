5fd16167aa4254abfc9e8f3b791ee3478abdae7c
INMD1 <lyw514549@gmail.com>
2024-08-14 03:29:42 +0900

    메인페이지 콘텐츠 디자인중

--- Frontend/lib/page/home.dart
+++ Frontend/lib/page/home.dart
@@ -21,72 +21,120 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          // 메뉴바 관련 코드
-          Expanded(
-            flex: 1, // 20%
-            child: Container(
-              margin: EdgeInsets.fromLTRB(8, 20, 8, 15),
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-              decoration: BoxDecoration(
-                color: Color(0xffeff3fb),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.menu, color: Colors.grey),
-                    onPressed: () {
-                      // 메뉴 버튼 클릭 시 실행할 코드
-                    },
-                  ),
-                  Text(
-                    'ALL LIFE',
-                    style: TextStyle(color: Colors.black, fontSize: 16.0),
-                  ),
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      'https://example.com/profile.jpg', // 여기에 프로필 이미지 URL 입력
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          // 콘텐츠 부분
-          Expanded(
-            flex: 8, // 60%
-            child: Container(
-              color: Colors.blueAccent, // 임시로 배경색 지정 (콘텐츠 영역)
-              child: Center(
-                child: Text(
-                  '콘텐츠 영역',
-                  style: TextStyle(fontSize: 24, color: Colors.white),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            // 메뉴바 관련 코드
+            Expanded(
+              flex: 1, // 20%
+              child: Container(
+                margin: EdgeInsets.fromLTRB(8, 20, 8, 15),
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                decoration: BoxDecoration(
+                  color: Color(0xffeff3fb),
+                  borderRadius: BorderRadius.circular(20),
                 ),
-              ),
-            ),
-          ),
-          // 하단 메뉴 부분
-          Expanded(
-            flex: 1, // 20%
-            child: Container(
-              child: Center(
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    _buildButton(0, Icons.home, "홈"),
-                    _buildButton(1, Icons.place, "지도 보기"),
-                    _buildButton(2, Icons.settings, "설정"),
-                    _buildButton(3, Icons.account_circle, "계정"),
+                    IconButton(
+                      icon: Icon(Icons.menu, color: Colors.grey),
+                      onPressed: () {
+                        // 메뉴 버튼 클릭 시 실행할 코드
+                      },
+                    ),
+                    Text(
+                      'ALL LIFE',
+                      style: TextStyle(color: Colors.black, fontSize: 16.0),
+                    ),
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        'https://example.com/profile.jpg', // 여기에 프로필 이미지 URL 입력
+                      ),
+                    ),
                   ],
                 ),
               ),
             ),
-          ),
-        ],
-      ),
+            // 콘텐츠 부분
+            Expanded(
+                flex: 8, // 60%
+                child: Container(
+                  color: Colors.blue,
+                  child:  Padding(padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
+                    child: Column(
+                      children: [
+                        //가벼운 인사말 정도로?
+                        Expanded(
+                            flex:1,
+                            child: Container(
+                              //옆으로 꽉 보이게
+                                width: double.infinity, // 임시로 배경색 지정 (콘텐츠 영역)
+                                child:Expanded(
+                                    flex: 1,
+                                    //긴딘힌 엡인사 표시
+                                    child:Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text("반갑습니다.",style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                                        Text("현재 있는 위치는 XXX동입니다.",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
+                                      ],
+                                    )
+                                )
+                            )
+                        ),
+                        //현재 지역에 있는 지진 대피 시설 안내
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Text"),
+                              Container(
+                              )
+                            ],
+                          ),
+                        )
+                        //현재 지역에 있는 해일 대피 시설 안내
+                        //현재 지역에 있는 기타 대피 시설 안내
+                        //아래에는 슬라읃 형식으로 정부에서 배포한 포스트 올리기
+                      ],
+                    ),
+                  ),
+                )
+            ),
+            // 하단 메뉴 부분
+            Expanded(
+              flex: 1, // 20%
+              child: Container(
+                decoration: BoxDecoration(
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    )
+                  ],
+                  color: Color(0xffF4F4F4),
+                ),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildButton(0, Icons.home, "홈"),
+                      _buildButton(1, Icons.place, "지도 보기"),
+                      _buildButton(2, Icons.settings, "설정"),
+                      _buildButton(3, Icons.account_circle, "계정"),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 
